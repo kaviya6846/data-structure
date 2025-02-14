f8c6a1fa5ce1594bf9e5494359747a9dbfa5d9ba
kaviya6846 <160532796+kaviya6846@users.noreply.github.com>
2025-02-14 09:59:55 +0530

    Create ds ex.java

---
+++ ds ex.java
@@ -0,0 +1,22 @@
+// "static void main" must be defined in a public class.
+public class Main {
+    public static void main(String[] args) {
+        
+        ArrayList<Integers array = new ArrayList();
+        array.add(10);
+        array.add(20);
+        array.add(28);
+        array.add(2,25);
+        array.remove(3);
+        array.remove(Integer.valueOf(25));
+        
+        
+        System.out.println(array.contains(26));
+        array,remove
+        
+        
+        for(int num: array){
+            System.out.println(num);
+            
+        }
+    }
